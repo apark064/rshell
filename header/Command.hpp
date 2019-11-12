6146f8509228dcfa5dd620a523ee0e58a8922b61
krikrifreeze <krikrifreeze@gmail.com>
2019-11-11 18:46:37 -0800

    made changes and added another constructor

--- header/Command.hpp
+++ header/Command.hpp
@@ -10,19 +10,22 @@
 #include "Exec.hpp"
 #include "Arg.hpp"
 
+using namespace std;
+
 class Command: public Word {
     protected:
-        std::string word;
+        string word;
     
     private:
-        std::vector<Word*> sequence;
+        vector<Word*> sequence;
 
     public:
         Command() {};
+	Command(vector<Word*> sequence){this->sequence = sequence;};
 	void execute(){
 		string line = "";
 		for (auto it = this->sequence.begin(); it != this->sequence.end(); it++){
-			line.append(it.word); //word is protected so it should be accessible
+			line.append(((*it)->word); //word is protected so it should be accessible
 			line.append(" "); //add a space
 		}
 		line.erase(line.end()); //delete the trailing space at the end
