0e04206b6073752dd559ad145def17c469cc4bc9
krikrifreeze <krikrifreeze@gmail.com>
2019-11-11 18:05:58 -0800

    Implemented Execute() Function for command. needs syntax review

--- header/Command.hpp
+++ header/Command.hpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 
+#include <unistd.h>
+
 #include "Word.hpp"
 #include "Exec.hpp"
 #include "Arg.hpp"
@@ -17,7 +19,16 @@ class Command: public Word {
 
     public:
         Command() {};
-	void execute(){};
+	void execute(){
+		string line = "";
+		for (auto it = this->sequence.begin(); it != this->sequence.end(); it++){
+			line.append(it.word); //word is protected so it should be accessible
+			line.append(" "); //add a space
+		}
+		line.erase(line.end()); //delete the trailing space at the end
+		char* args[] = {"/bin/bash", "-c", line, NULL};
+        	execvp(args[0], args);
+	};
         
 };
 #endif //__COMMAND_HPP__
